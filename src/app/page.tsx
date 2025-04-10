67a47d78627ed611119b8ad93b5b73b00ced8df8
munaciella <franes88@gmail.com>
2025-04-10 15:33:18 +0100

    implement shadcn ui in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1 className="text-red-600">Resume-AI</h1>
+      <Button variant="outline" className="cursor-pointer">Click me</Button>
     </main>
   )
 }
